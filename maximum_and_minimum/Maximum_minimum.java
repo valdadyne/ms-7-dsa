5b4ce82cf9e304ef1b0c5735d2452506be58a092
Gitau 'Valdadyne' Rodgers <rodgers.gitau@gmail.com>
2016-07-25 09:26:13 +0300

    Update Maximum_minimum.java

--- maximum_and_minimum/Maximum_minimum.java
+++ maximum_and_minimum/Maximum_minimum.java
@@ -9,3 +9,16 @@
 // put the lowest in its array 
 // put the highest in its array
 // output the highest then output the lowest
+
+Public class MinAndMax{
+  Public static void main(String[]args){
+    int numArray= int[]{-10,11,3,21,97,34,30};
+    int maxNum=numArray[0];
+    int minNum=numArray[0];
+    for (int i=0;i<numArray.length;i++){
+      if (numArray[i]>maxNum) maxNum=numArray[i]
+      else if(numArray[i]<minNum) maxNum=numArray[i]
+    }
+    system.out.println(maxNum,minNum);
+  }
+}
