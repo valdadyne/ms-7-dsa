5ac47744e62225081b8316e5603940c7335e7d0b
Jeffrey Mutula <kjrmutula@gmail.com>
2016-07-25 09:37:01 +0300

    jeff solution

--- maximum_and_minimum/Maximum_minimum.java
+++ maximum_and_minimum/Maximum_minimum.java
@@ -2,42 +2,31 @@
 
 //Create your own array of integers
 // Find the maximum and minimum of this array that you just created.
-// create a variable to hod the maximum
+// create a variable to hold the maximum
 // create a variable to hold the minimum
 // take in the array of numbers
 // begin to loop through them
 // put the lowest in its array 
 // put the highest in its array
 // output the highest then output the lowest
-'''java solution
-```java
-
-public class Third {
-
-	public String ThirdGreatest(String[]strArr){
-		
-		String strValue = "" , output="";
-		boolean sort = true;
-		while(sort){//beginning of bubble sort
-			
-			for(int i=0; i<strArr.length;i++){//loop thru the array
-				if(strArr[i].length() <  
-						strArr.length[i+1].length()){
-					strValue=strArr[i];
-					strArr[i]=strArr[i+1];
-					strArr[i+1]=strValue;
-					sort=false;
-					
-				}
-				
-			}/
-			
-		}//end of looping array
-	}//end of sort
-	if(strArr.length < 3 && str.length > 0){
-		output = strArr [strArr.lenth-1];
-	}else{
-		output = strArr[2];
+public static void main(String[] args){
+		int[] arrayIn = {56,9,21,78,91,64,84,53,25};
+		Arrays.sort(arrayIn);
+		int min = arrayIn[0];
+		int max = arrayIn[arrayIn.length-1];
+                
+                int[] minA = new int[1];
+                int[] maxA = new int[1];
+                
+                for(int i = 0; i <= arrayIn.length - 1; i++){
+                    if (arrayIn[i] <= max){
+                        maxA[0] = arrayIn[i];
+                    }
+                    if (arrayIn[i] <= min){
+                        minA[0] = arrayIn[i];
+                    }
+                }
+                System.out.println(Arrays.toString(arrayIn));
+                System.out.println("Max integer is " + Arrays.toString(maxA));
+                System.out.println("Min integer is " + Arrays.toString(minA));
 	}
-	return routput;
-}
